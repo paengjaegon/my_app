68b33c3bd1ab047e578cf4b1321f853a9b3a61ca
paengjaegon <mission19@naver.com>
2015-09-30 15:32:28 +0900

    EJS TEST

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ var path = require('path');
 var app = express();
 
 
+app.set("view engine", 'ejs');
+
 //app.use(express.static(__dirname+'/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//console.log(__dirname);
+
+var data={count:0};
+app.get('/', function (req, res){
+  res.render('my_first_ejs');
+});
+app.get('/reset', function(req,res){
+  data.count=0;
+  res.render('my_fist_ejs', data);
+});
+
+app.get('set/count', function(req,res){
+  if(req.query.count) data.count=req.queyr.count;
+  res.render('my_first_ejs', data);
+});
+
+app.get('set/:num', function(req, res){
+  data.count=req.params.num;
+  res.render('my_fist_ejs', data);
+});
 
 app.listen(3000, function(){
   console.log('Server On!');
